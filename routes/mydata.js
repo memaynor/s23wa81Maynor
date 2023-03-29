a029abe748cd50670824d196cf362da64004e25b
memaynor <78869428+memaynor@users.noreply.github.com>
2023-03-29 13:12:14 -0500

    idk anymore i think i did it wrong

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET my data. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Max Maynor' });
+  res.render('mydata', { title: 'Max Maynor' });
 });
 
 module.exports = router;
